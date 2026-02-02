850fe4e3bb0cbaab735fb4dc9f4f3c0097d1c19d
satishmadhunala1 <satishmadhunala2001@gmail.com>
2026-02-02 12:20:27 +0530

    changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -48,7 +48,7 @@ api.interceptors.response.use(
     if (error.response?.status === 401) {
       localStorage.removeItem("authToken");
       localStorage.removeItem("user");
-      window.location.href = "/signin";
+      window.location.href = "/";
     }
 
     return Promise.reject(error);
