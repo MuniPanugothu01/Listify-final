9d1b1bb7927c5506623da50bd361269b647bf20a
satishmadhunala1 <satishmadhunala2001@gmail.com>
2026-02-25 10:49:18 +0530

    changes in dot env path

--- server/services/googleAuth.OAuth.js
+++ server/services/googleAuth.OAuth.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 const { OAuth2Client } = require("google-auth-library");
 const { logger } = require("../utils/logger");
 const User = require("../models/User");
